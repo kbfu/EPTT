01e22aa6f107bb4769eeba03e7ad37de22f0d3b5
zhangdonghao <d.zeta@163.com>
2017-07-17 14:41:47 +0800

    add test server

--- mp.py
+++ mp.py
@@ -7,29 +7,31 @@ import asyncio
 
 async def async_send():
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://www.chinaso.com') as resp:
-            # await asyncio.sleep(1)
-            try:
+        try:
+            async with session.get('http://127.0.0.1:60001/get') as resp:
                 await resp.text()
-                assert resp.status == 200
-            except AssertionError as e:
-                print(e)
+                try:
+                    assert resp.status == 200
+                except AssertionError:
+                    print(resp.status)
+        except Exception as e:
+            print(e)
 
 
 def run(n):
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_event_loop()
-    print('async')
-    now = datetime.timestamp(datetime.now())
     loop.run_until_complete(asyncio.gather(
         *[async_send() for _ in range(n)]
     ))
-    done = datetime.timestamp(datetime.now()) - now
-    print(done)
 
 
 if __name__ == '__main__':
-    with Pool(processes=8) as pool:
-        results = [pool.apply_async(run, (500, )) for _ in range(8)]
+    print('double process with uvloop')
+    now = datetime.timestamp(datetime.now())
+    with Pool(processes=2) as pool:
+        results = [pool.apply_async(run, (50, )) for _ in range(80)]
         for r in results:
             r.wait()
+    done = datetime.timestamp(datetime.now()) - now
+    print(done)
