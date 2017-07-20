e82cf8e32db03efb593b6d248a667d9e77d53512
zhangdonghao <d.zeta@163.com>
2017-07-20 10:35:15 +0800

    refine

--- src/ept/core/csv_processor.py
+++ src/ept/core/csv_processor.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import csv
+import os
 
 
 def write(file_name, *args):
@@ -11,3 +12,17 @@ def write(file_name, *args):
         writer = csv.writer(f)
         writer.writerow([args[0], args[1], args[2], args[3], args[4],
                          args[5], args[6], args[7]])
+
+
+def remove(file_name):
+    try:
+        os.remove(file_name)
+    except FileNotFoundError:
+        pass
+
+
+def create(file_name):
+    with open(file_name, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['name', 'resp', 'headers', 'status_code',
+                         'elapsed_time', 'start_time', 'end_time', 'exception'])
