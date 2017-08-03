0d4647474fc61c5a4c026d11998cf970feb45d02
zhangdonghao <d.zeta@163.com>
2017-08-03 11:46:37 +0800

    remove multiprocessing

--- src/ept/core/http.py
+++ src/ept/core/http.py
@@ -9,19 +9,20 @@ async def get(sem, session, log_manager, args):
     '''
     start = date_util.timestamp_now()
     try:
-        async with session.get(args[0]) as resp:
-            resp_text = await resp.text()
-            end = date_util.timestamp_now()
-            elapsed = end - start
-            log_manager.update({'name': args[1], 'resp': resp_text,
-                                'headers': json.dumps(dict(resp.headers)),
-                                'status': resp.status, 'elapsed': elapsed,
-                                'start': start, 'end': end,
-                                'exception': ''})
+        async with sem:
+            async with session.get(args[0]) as resp:
+                resp_text = await resp.text()
+                end = date_util.timestamp_now()
+                elapsed = end - start
+                log_manager.append({'name': args[1], 'resp': resp_text,
+                                    'headers': json.dumps(dict(resp.headers)),
+                                    'status': resp.status, 'elapsed': elapsed,
+                                    'start': start, 'end': end,
+                                    'exception': ''})
     except Exception as exc:
         end = date_util.timestamp_now()
         elapsed = end - start
-        log_manager.update({'name': args[1], 'resp': '',
+        log_manager.append({'name': args[1], 'resp': '',
                             'headers': json.dumps(dict(resp.headers)),
                             'status': resp.status, 'elapsed': elapsed,
                             'start': start, 'end': end,
